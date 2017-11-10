ee4b63a89db4aec58708bc5dd7bfb199d843d05b
JiaCong <18851280888@163.com>
2017-11-10 11:55:55 +0800

    add websocket && initialize flux

---
+++ plugins/services/src/js/events/ConsoleActions.js
@@ -0,0 +1,90 @@
+import AppDispatcher from "#SRC/js/events/AppDispatcher";
+
+import {
+  REQUEST_CONSOLE_CONNECT,
+  REQUEST_CONSOLE_DISCONNECT,
+  REQUEST_CONSOLE_MESSAGE_ERROR,
+  REQUEST_CONSOLE_MESSAGE_SUCCESS
+} from "../constants/ActionTypes";
+
+var ws;
+
+function getConsoleURL(task) {
+  var httpsEnabled = window.location.protocol == "https:";
+  return `ws://${window.location.host}/terminal/375e9a1b2b58/ws`;
+};
+
+function sendMessage(ws, type, content) {
+  var message = JSON.stringify({
+    type: type,
+    content: content
+  })
+  if(ws.readyState != 3) {
+      ws.send(message)
+  }
+};
+
+var ConsoleActions = {
+  consoleConnect: function(task) {
+    ws = new WebSocket(getConsoleURL(task));
+
+    ws.onopen = function(event) {
+      sendMessage(ws, 'init', JSON.stringify({ Arguments: "", AuthToken: ""}));
+      // pingTimer = setInterval(sendPing, 30 * 1000, ws);
+      AppDispatcher.dispatch({
+        type: REQUEST_CONSOLE_CONNECT,
+        data: "Connect success"
+      });
+    };
+
+    ws.onmessage = function(event) {
+      // console.log(event)
+      var data = JSON.parse(event.data);
+      switch(data.type) {
+        case 'output':
+          // decode message and convert to utf-8
+          // term.io.writeUTF8(window.atob(data.content));
+
+          break;
+        case 'pong':
+          // pong
+          break;
+        case 'set-title':
+          // term.setWindowTitle(data.content);
+          break;
+        case 'set-preferences':
+          // var preferences = JSON.parse(data.content);
+          // Object.keys(preferences).forEach(function(key) {
+          //     console.log("Setting " + key + ": " +  preferences[key]);
+          //     term.getPrefs().set(key, preferences[key]);
+          // });
+          break;
+        case 'set-autoreconnect':
+          // autoReconnect = JSON.parse(data.content);
+          // console.log("Enabling reconnect: " + autoReconnect + " seconds")
+          break;
+        case 'error':
+          // term.io.writeUTF8(window.atob(data.content));
+          break;
+        default:
+          console.log("new websocket", url)
+          // unidentified message
+          // term.io.writeUTF8("Invalid message: " + event.data);
+      }
+    };
+
+    ws.onclose = function(event) {
+      // if (term) {
+      //     term.uninstallKeyboard();
+      //     term.io.showOverlay("Connection Closed", null);
+      // }
+      // clearInterval(pingTimer);
+      // if (autoReconnect > 0) {
+      //     setTimeout(openWs, autoReconnect * 1000);
+      // }
+      console.log("WebSocket close");
+    }
+  }
+};
+
+module.exports = ConsoleActions;
--- plugins/services/src/js/pages/task-details/TaskConsoleTab.js
+++ plugins/services/src/js/pages/task-details/TaskConsoleTab.js
@@ -3,41 +3,171 @@ import { routerShape } from "react-router";
 import mixin from "reactjs-mixin";
 import { StoreMixin } from "mesosphere-shared-reactjs";
 import Task from "../../structs/Task";
+import ConsoleStore from "../../stores/ConsoleStore";
+
+const METHODS_TO_BIND = [
+  "listFiles",
+  "catFile",
+  "showHelp",
+  "handleInput",
+  "clearInput",
+  "addHistory",
+  "handleClick",
+  "clearHistory"
+];
 
 class TaskConsoleTab extends mixin(StoreMixin) {
   constructor() {
     super(...arguments);
     this.state = {
-      isLoading: true
+      commands: {},
+      history: [],
+      prompt: '$ '
     };
+
+    METHODS_TO_BIND.forEach(method => {
+      this[method] = this[method].bind(this);
+    });
   }
 
   componentWillMount() {
     super.componentWillMount();
+
+    const { task } = this.props;
+    ConsoleStore.connectConsole(task)
   }
 
   componentWillUnmount() {
     super.componentWillUnmount();
   }
 
-  render() {
-    const location = "http://localhost:21888/terminal/eb6b6eab9130";
-    const { task } = this.props;
-    console.log(task);
-
-    return (
-      <div className="iframe-page-container">
-        <iframe
-          width="100%"
-          height="100%"
-          frameBorder="0"
-          id="console-iframe"
-          src={location}
-        />
-      </div>
-    );
+  clearHistory() {
+    this.setState({ history: [] });
   }
-}
+
+  registerCommands() {
+    this.setState({
+      commands: {
+        'clear' : this.clearHistory,
+        'ls'    : this.listFiles,
+        'intro' : this.showWelcomeMsg,
+        'help'  : this.showHelp,
+        'cat'   : this.catFile,
+        'source': this.openLink('https://github.com/prakhar1989/react-term/blob/master/src/app.js'),
+        'github': this.openLink('http://github.com/prakhar1989'),
+        'blog'  : this.openLink('http://prakhar.me'),
+        'resume': this.openLink('https://github.com/prakhar1989/cv/blob/master/Resume.pdf')
+      }
+    });
+  }
+
+  listFiles() {
+    this.addHistory("README.md");
+  }
+
+  showWelcomeMsg() {
+      this.addHistory("Hello, I'm Prakhar Srivastav, a graduate student in the Computer Science department (Machine Learning track).");
+      this.addHistory("Type `help` to see what all commands are available");
+  }
+
+  catFile(arg) {
+      if (arg === "README.md") {
+          this.addHistory('### REACT TERM');
+          this.addHistory("A couple of days back, I got an email from Columbia (the university that I'm stated to join) informing me that my new email ID and other student IT services were ready. Hosting my own webpage on a university's domain had long been a wish of mine, so as soon as I learnt about having some server space on the university's server I got excited wanted to put something interesting. Since I already have " +
+                          "a boring about me page, I went " +
+                          "with something different and built a simple terminal emulator in React!");
+          this.addHistory("type `source` to view the source code");
+      } else {
+          this.addHistory("cat: " +  arg + ": No such file or directory");
+      }
+  }
+  openLink(link) {
+      return function() {
+        window.open(link, '_blank');
+      }
+  }
+
+  showHelp() {
+      this.addHistory("help - this help text");
+      this.addHistory("github - view my github profile");
+      this.addHistory("source - browse the code for this page");
+      this.addHistory("intro - print intro message");
+      this.addHistory("blog - read some stuff that I've written");
+      this.addHistory("clear - clear screen");
+      this.addHistory("cat - print contents of a file");
+      this.addHistory("ls - list files");
+      this.addHistory("resume - view my resume");
+  }
+
+  componentDidMount() {
+    this.registerCommands();
+    this.showWelcomeMsg();
+    this.refs.term.focus();
+  }
+
+  // componentDidUpdate() {
+  //     var el = React.findDOMNode(this);
+  //     //var container = document.getElementsByClassName('container')[0];
+  //     // var container = document.getElementById("main");
+  //     container.scrollTop = el.scrollHeight;
+  // }
+
+  handleInput(e) {
+    if (e.key === "Enter") {
+      var input_text = this.refs.term.value;
+      var input_array = input_text.split(' ');
+      var input = input_array[0];
+      var arg = input_array[1];
+      var command = this.state.commands[input];
+
+      this.addHistory(this.state.prompt + " " + input_text);
+
+      if (command === undefined) {
+          this.addHistory("sh: command not found: " + input);
+      } else {
+          command(arg);
+      }
+      console.log(this)
+      this.clearInput();
+    }
+  }
+
+  clearInput() {
+      this.term.t().value = "";
+  }
+
+  addHistory(output) {
+    var history = this.state.history;
+    history.push(output)
+    this.setState({
+      'history': history
+    });
+  }
+
+  handleClick() {
+    // console.log(this)
+    this.refs.term.focus();
+  }
+
+  render() {
+    // const location = "http://localhost:21888/terminal/eb6b6eab9130";
+    // const { task } = this.props;
+    // console.log(task);
+    var output = this.state.history.map(function(op, i) {
+        return <p key={i}>{op}</p>
+    });
+    return (
+      <div className='input-area iframe-page-container' onClick={this.handleClick.bind(this)}>
+        {output}
+        <p>
+          <span className="prompt">{this.state.prompt}</span> 
+          <input type="text" onKeyPress={this.handleInput.bind(this)} ref="term"/>
+        </p>
+      </div>
+    )
+  }
+
+};
 
 TaskConsoleTab.contextTypes = {
   params: React.PropTypes.object,
